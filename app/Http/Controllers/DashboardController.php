dc9176fe29048ed335d788d1af6cdfda5861d888
SophanonChhoun <sophanon.chhoun@student.niptict.edu.kh>
2021-03-30 14:47:14 +0700

    [TASK]dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\admin\Category;
+use App\Models\admin\Course;
+use App\Models\admin\CourseLesson;
+use App\Models\admin\Customer;
+use App\Models\admin\CustomerCourse;
+use App\Models\admin\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view("admin.dashboard.dashboard");
+        $users = User::all()->count();
+        $customers = Customer::all()->count();
+        $categories = Category::all()->count();
+        $courses = Course::all()->count();
+        $lessons = CourseLesson::all()->count();
+        $enrollCourses = CustomerCourse::all()->count();
+
+
+        return view("admin.dashboard.dashboard", compact('courses', 'customers', 'users', 'categories', 'enrollCourses', 'lessons'));
     }
 }
--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -1,179 +1,78 @@
 @extends('admin.layout.default')
 
-{{--@section("content")--}}
-{{--    <div class="container-fluid">--}}
-{{--        <h1 class="mt-4">Dashboard</h1>--}}
-{{--        <ol class="breadcrumb mb-4">--}}
-{{--            <li class="breadcrumb-item active">Dashboard</li>--}}
-{{--        </ol>--}}
+@section("content")
+    <div class="container-fluid">
+        <h1 class="mt-4">Dashboard</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
 
-{{--        <div class="">--}}
-{{--            <div class="row">--}}
-{{--                <div class="col-md-3">--}}
-{{--                    <div class="card-counter success">--}}
-{{--                        <i class="fa fa-key"></i>--}}
-{{--                        <span class="count-numbers">  {{$room_available }}</span>--}}
-{{--                        <a href="/admin/rooms/list/1" style="color:white;">--}}
-{{--                            <span class="count-name">Room Available</span>--}}
-{{--                        </a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+        <div class="">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card-counter success">
+                        <i class="fa fa-key"></i>
+                        <span class="count-numbers">  {{$users ?? null }}</span>
+                        <a href="/admin/user/list" style="color:white;">
+                            <span class="count-name">Users</span>
+                        </a>
+                    </div>
+                </div>
 
-{{--                <div class="col-md-3">--}}
-{{--                    <div class="card-counter warning">--}}
-{{--                        <i class="fa fa-calendar"></i>--}}
-{{--                        <span class="count-numbers">--}}
-{{--                      {{ $check_in }}</span>--}}
-{{--                        <span class="count-name">Check-In</span>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
+                <div class="col-md-4">
+                    <div class="card-counter warning">
+                        <i class="fa fa-key"></i>
+                        <span class="count-numbers">  {{$customers ?? null }}</span>
+                        <a href="/admin/customer/list" style="color:white;">
+                            <span class="count-name">Customers</span>
+                        </a>
+                    </div>
+                </div>
 
-{{--                <div class="col-md-3">--}}
-{{--                    <div class="card-counter danger">--}}
-{{--                        <i class="fa fa-usd"></i>--}}
-{{--                        <span class="count-numbers">{{ $check_out }}</span>--}}
-{{--                        <span class="count-name">Check-Out</span>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="col-md-3">--}}
-{{--                    <div class="card-counter info">--}}
-{{--                        <i class="fa fa-bookmark"></i>--}}
-{{--                        <span class="count-numbers">--}}
-{{--                     {{ $room_booked }}  </span>--}}
-{{--                        <a href="/admin/rooms/list/0">--}}
-{{--                            <span class="count-name">Room Booked</span>--}}
-{{--                        </a>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="">--}}
-{{--                <div class="row">--}}
+                <div class="col-md-4">
+                    <div class="card-counter danger">
+                        <i class="fa fa-key"></i>
+                        <span class="count-numbers">  {{$categories ?? null }}</span>
+                        <a href="/admin/category/list" style="color:white;">
+                            <span class="count-name">Categories</span>
+                        </a>
+                    </div>
+                </div>
 
-{{--                    <div class="col-md-3">--}}
+                <div class="col-md-4">
+                    <div class="card-counter info">
+                        <i class="fa fa-key"></i>
+                        <span class="count-numbers">  {{$courses ?? null }}</span>
+                        <a href="/admin/course/list" style="color:white;">
+                            <span class="count-name">Courses</span>
+                        </a>
+                    </div>
+                </div>
 
-{{--                        <div class="card-counter primary">--}}
-{{--                            <i class="fa fa-home"></i>--}}
-{{--                            <span class="count-numbers">  {{$total_room }}</span>--}}
-{{--                            <a href="/admin/rooms/list" style="color:white;"><span class="count-name">Room</span></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
+                <div class="col-md-4">
+                    <div class="card-counter siliver">
+                        <i class="fa fa-bookmark"></i>
+                        <span class="count-numbers"> {{ $lessons ?? null }}  </span>
+                        <a href="/admin/lesson/list">
+                            <span class="count-name">Lessons</span>
+                        </a>
+                    </div>
+                </div>
 
-{{--                    <div class="col-md-3">--}}
-{{--                        <div class="card-counter siliver">--}}
-{{--                            <i class="fa fa-archive"></i>--}}
-{{--                            <span class="count-numbers">--}}
-{{--                      {{ $room_type }}</span>--}}
-{{--                            <a href="/admin/room_type/list" style="color:white;"><span class="count-name">Room Type</span></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
+                <div class="col-md-4">
+                    <div class="card-counter orange">
+                        <i class="fa fa-bookmark"></i>
+                        <span class="count-numbers">
+                         {{ $enrollCourses ?? null }}  </span>
+                        <span class="count-name">Enroll Courses</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-{{--                    <div class="col-md-3">--}}
-{{--                        <div class="card-counter orange">--}}
-{{--                            <i class="fa fa-users" aria-hidden="true"></i>--}}
-{{--                            <span class="count-numbers">{{ $customer_today }}</span>--}}
-{{--                            <a href="/admin/customer/list" style="color:white"><span class="count-name">New Customers</span></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-md-3">--}}
-{{--                        <div class="card-counter secondary">--}}
-{{--                            <i class="fa fa-money"></i>--}}
-{{--                            <span class="count-numbers">--}}
-{{--                        {{$payment_today }}</span>--}}
-{{--                            <a href="/admin/payments/list" style="color:white"><span class="count-name">Payment</span></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-
-{{--                <form action="{{ url('/admin/report/show') }}" method="get">--}}
-{{--                    <div class="row">--}}
-{{--                            <div class="col-xs-4 col-lg-4" >--}}
-{{--                                <div class="form-group">--}}
-{{--                                    <label>From :</label>   <input type="date" id="start_date"  name="start_date" >--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                            <div class="col-xs-4 col-lg-4">--}}
-{{--                                <div class="form-group">--}}
-{{--                                    <label>To :</label>  <input type="date"  id="end_date" name="end_date">--}}
-{{--                                </div></div>--}}
-{{--                            <div class="col-xs-4 col-lg-4">--}}
-{{--                                <input class="btn btn-primary" type="submit" value="show Report">--}}
-{{--                            </div>--}}
-{{--                    </div>--}}
-{{--                </form>--}}
-{{--                <div id="top_x_div" class="p-3 mb-2" style="height: 600px; width: 100%"></div>--}}
-{{--                <h1 class="p-3 mb-2">Recently Booking</h1>--}}
-{{--                    <table class="table table-bordered "  id="table_id">--}}
-{{--                        <tr>--}}
-{{--                            <th>ID</th>--}}
-{{--                            <th>Customer</th>--}}
-{{--                            <th>Hotel</th>--}}
-{{--                            <th>Booking Type</th>--}}
-{{--                            <th>Check in</th>--}}
-{{--                            <th>Check out</th>--}}
-{{--                            <th>Status</th>--}}
-{{--                            <th colspan="2" class="text-center">Action</th>--}}
-{{--                        </tr>--}}
-{{--                        <div class="col-md-5"></div>--}}
-{{--                        @forelse($data as $booking)--}}
-{{--                            <tr>--}}
-{{--                                <td>{{ $booking->id }}</td>--}}
-{{--                                <td>--}}
-{{--                                    <a href="{{ url('/admin/bookings/show/payment/'.$booking->id) }}">{{ $booking->customer->last_name ?? null }} {{ $booking->customer->first_name ?? null }}</a>--}}
-{{--                                </td>--}}
-{{--                                <td>{{ $booking->hotel->name }}</td>--}}
-{{--                                <td>{{ $booking->booking_type->name }}</td>--}}
-{{--                                <td>{{ $booking->check_in_date }}</td>--}}
-{{--                                <td>{{ $booking->check_out_date }}</td>--}}
-{{--                                <td>--}}
-{{--                                    <input type="checkbox" data-toggle="modal" data-target="#status{{ $booking->id }}" @if($booking->is_enable) checked @endif>--}}
-{{--                                    @include("admin.booking.status")--}}
-{{--                                </td>--}}
-{{--                                <td><a href="/admin/bookings/show/{{ $booking->id }}" class="btn btn-warning">Edit</a></td>--}}
-{{--                                <td>--}}
-{{--                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal{{ $booking->id }}">Delete</button>--}}
-{{--                                    @include('admin.booking.delete')--}}
-{{--                                </td>--}}
-{{--                                @empty--}}
-{{--                                    <td colspan="5" class="text-center">There is no value</td>--}}
-{{--                            </tr>--}}
-{{--                        @endforelse--}}
-{{--                    </table>--}}
-{{--    </div>--}}
-
-{{--@endsection--}}
+@endsection
 
 @section("script")
                 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-{{--                <script type="text/javascript">--}}
-{{--                    google.charts.load('current', {'packages':['bar']});--}}
-{{--                    google.charts.setOnLoadCallback(drawStuff);--}}
-
-{{--                    function drawStuff() {--}}
-{{--                        var data = new google.visualization.arrayToDataTable([--}}
-{{--                            ['Room Type', 'Money'],--}}
-{{--                            @foreach($roomTypes as $roomType)--}}
-{{--                                ['{{$roomType->name}}',{{$roomType->total}}],--}}
-{{--                            @endforeach--}}
-{{--                        ]);--}}
-
-{{--                        var options = {--}}
-{{--                            width: 800,--}}
-{{--                            legend: { position: 'none' },--}}
-{{--                            chart: {--}}
-{{--                                title: 'Room type',--}}
-{{--                                subtitle: 'money get by each room type' },--}}
-{{--                            axes: {--}}
-{{--                                x: {--}}
-{{--                                    0: { side: 'top', label: 'Currency dollar'} // Top x-axis.--}}
-{{--                                }--}}
-{{--                            },--}}
-{{--                            bar: { groupWidth: "90%" }--}}
-{{--                        };--}}
-
-{{--                        var chart = new google.charts.Bar(document.getElementById('top_x_div'));--}}
-{{--                        // Convert the Classic options to Material options.--}}
-{{--                        chart.draw(data, google.charts.Bar.convertOptions(options));--}}
-{{--                    };--}}
-{{--                </script>--}}
-
 @endsection
